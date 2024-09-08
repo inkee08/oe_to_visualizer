5698a058fc02c874fc7b3e499a1d438c7fde800b
inkee <inkee1234@gmail.com>
2024-09-08 10:06:25 -0400

    fix trailing 0s in flow by weight list

--- oe to visualizer json.py
+++ oe to visualizer json.py
@@ -2,16 +2,26 @@ import json
 from datetime import datetime, timezone
 from collections import defaultdict
 
-json_file = "oe-test.json"
+json_dir = "test/"
+json_file = "oe-scale-only.json"
+json_path = json_dir + json_file
 
-with open(json_file) as user_file:
+with open(json_path) as user_file:
   parsed_json = json.load(user_file)
 
 data = defaultdict(dict)
 def translate():
+  
+  highest = {
+  'weight' : len(parsed_json['weight']),
+  'flows' : len(parsed_json['flows']),
+  'pressures' : len(parsed_json['pressures']),
+}
+  elapsed = max(highest)
+
   # elapsed
   data['elapsed'] = []
-  for i in parsed_json['flows']:
+  for i in parsed_json[elapsed]:
     data['elapsed'].append(str(i['x']))
     
   # weight
@@ -28,6 +38,19 @@ def translate():
   data['flow']['by_weight'] = []
   for i in parsed_json['flows']:
     data['flow']['by_weight'].append(str(i['y']))
+    
+  fix_issues()
+  
+def fix_issues():
+  
+  # delete ending 0s in flow list
+  count = 0
+  for i in reversed(data['flow']['by_weight']):
+    if float(i) == 0:
+      count += 1
+    if float(i) > 0:
+      break
+  data['flow']['by_weight'] = data['flow']['by_weight'][:-count]
 
 # from Odyssey Json file
 name = f"Argos {parsed_json['name']}"
@@ -39,7 +62,7 @@ roastery = f"{parsed_json.get('roastery')} {parsed_json.get('beans')}"
 dosage = parsed_json.get('dosage', 'null')
 grinder_brand = parsed_json.get('grindBrand', '')
 grinder_model = parsed_json.get('grindModel', '')
-grinder_setting = str(parsed_json.get('grindSetting'), '')
+grinder_setting = str(parsed_json.get('grindSetting', ''))
 if grinder_setting == "None":
   grinder_setting = ""
 drink_weight = parsed_json.get('highestScaleWeight')
@@ -72,7 +95,7 @@ if __name__ == "__main__":
   
   translate()
 
-  visualizer =  data | profile
-
+  visualizer =  profile | data
+  
   with open(f"visualizer_{json_file}", "w") as file:
     file.write(json.dumps(visualizer, indent=2))
